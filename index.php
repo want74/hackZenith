08fecaa513665f260f2f3cabe95366c77aa6a39d
ListraiD <kola130704@gmail.com>
2021-11-27 04:21:43 +0900

    свёрстана первая страница

--- index.php
+++ index.php
@@ -3,12 +3,33 @@
 <html>
 <head>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link rel="stylesheet" href="css/style.css">
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title></title>
+	<style>
+		body {
+			background-image: url('../img/girya.png');
+		}
+	</style>
 </head>
 <body>
-
+	<div class="col-5">
+		<div class="row" style="margin-top:35vh;">
+			<div class="col-10 ml-auto">
+				<div class="row">
+					<p class="tagline ml-3">Слоган</p>
+				</div>
+				<div class="row mt-4">
+					<p class="textFirstPage ml-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam turpis tellus, mollis eget fermentum ac, lacinia eu leo. Maecenas nec leo posuere, molestie dolor ut, aliquet ex. Phasellus ac dolor ornare neque congue eleifend. Donec ultrices euismod pretium. Integer mattis et dui in vestibulum. Aenean placerat erat nulla, vel blandit sem commodo et. Nunc convallis a ligula non fermentum.</p>
+				</div>
+				<div class="row mt-4">					
+					<button type="button" class="btn btn-reg mx-2">Регистрация</button>
+					<button type="button" class="btn btn-vhod mx-3">Войти</button>
+				</div>
+			</div>
+		</div>
+	</div>
 </body>
 </html>
 
