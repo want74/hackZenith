d5cb27447ce153fa5dff229cc9bbb7a3db119140
ListraiD <kola130704@gmail.com>
2021-11-27 19:59:37 +0900

    правки после армагедона

---
+++ game2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+	<style type="text/css">
+		.header-botgy{
+			background:#515A82 ;
+		}
+	</style>
+</head>
+<body>
+	<div class="col-12">
+		
+	</div>
+
+</body>
+</html>
